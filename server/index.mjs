4dd431ab9dcf4b62b9762339db7c918fe80ad88f
ryiianqueiroz <ryanqueirozdev44@gmail.com>
2024-08-23 17:27:02 -0300

    update options

--- server/index.mjs
+++ server/index.mjs
@@ -16,6 +16,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).json({ error: err.message });
+  } else {
+    next();
+  }
+});
+
+
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
